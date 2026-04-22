088b26aa3d0941408d24a9f7c7f5543f6bfc4133
chanlapping <chanlapping64@gmail.com>
2026-04-22 19:34:21 +0800

    connect to MongoDB

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
 const express = require("express");
+const Note = require("./models/note");
+
 const app = express();
 
 let notes = [
@@ -29,17 +32,15 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (req, res) => {
-  const id = req.params.id;
-  const note = notes.find((n) => n.id === id);
-  if (note) {
+  Note.findById(req.params.id).then((note) => {
     res.json(note);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/notes/:id", (req, res) => {
@@ -48,24 +49,18 @@ app.delete("/api/notes/:id", (req, res) => {
   res.status(204).end();
 });
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
-
 app.post("/api/notes", (req, res) => {
   const body = req.body;
   if (!body.content) {
     return res.status(400).json({ error: "content missing" });
   }
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
-  notes = notes.concat(note);
-  res.json(note);
+  });
+  note.save().then((savedNote) => {
+    res.json(savedNote);
+  });
 });
 
 const unknownEndpoint = (req, res) => {
